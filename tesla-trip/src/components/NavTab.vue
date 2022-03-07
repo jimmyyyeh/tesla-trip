6ba7e240ba680854bd893c5446c86bf9c9ae4a74
Jimmy Yeh <chienfeng0719@gmail.com>
2022-03-07 16:25:43 +0800

    Update nav-tab style

--- tesla-trip/src/components/NavTab.vue
+++ tesla-trip/src/components/NavTab.vue
@@ -1,5 +1,5 @@
 <template>
-  <van-tabs class="nav-tab" color="#000000" background="#F0F0F0" v-model:active="tabName" @click-tab="redirectPage">
+  <van-tabs class="nav-tab" v-model:active="tabName" @click-tab="redirectPage">
     <van-tab title="首頁" name="home"></van-tab>
     <van-tab title="旅途里程" name="trip"></van-tab>
     <van-tab title="充電站景點" name="attraction"></van-tab>
