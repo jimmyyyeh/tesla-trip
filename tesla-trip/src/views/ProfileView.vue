4f618f7d905b1e0ef662ad2b0e02cd8569853adf
Jimmy Yeh <chienfeng0719@gmail.com>
2022-03-07 18:20:29 +0800

    Get profile on profile page mounted

--- tesla-trip/src/views/ProfileView.vue
+++ tesla-trip/src/views/ProfileView.vue
@@ -5,28 +5,28 @@
       <div class="profile-form">
         <div class="username column">
           <label class="input-label">使用者名稱:</label>
-          <label class="input-label">{{ user.username }}</label>
+          <label class="input-label">{{ profile.username }}</label>
         </div>
         <div class="birthday column">
           <label class="input-label">生日:</label>
-          <label class="input-label">{{ user.birthday }}</label>
+          <label class="input-label">{{ profile.birthday }}</label>
         </div>
         <div class="sex column">
           <label class="input-label">性別:</label>
-          <label class="input-label">{{ user.sex === 1 ? '男' : '女' }}</label>
+          <label class="input-label">{{ profile.sex === 1 ? '男' : '女' }}</label>
         </div>
         <div class="point column">
           <label class="input-label">積分:</label>
-          <label class="input-label">{{ user.point }}</label>
+          <label class="input-label">{{ profile.point }}</label>
         </div>
         <hr>
         <div class="nickname column">
           <label class="input-label">暱稱:</label>
-          <input type="text" v-model="user.nickname">
+          <input type="text" v-model="profile.nickname">
         </div>
         <div class="email column">
           <label class="input-label">電子郵件:</label>
-          <input type="text" v-model="user.email">
+          <input type="text" v-model="profile.email">
         </div>
         <div class="button-group">
           <button class="default-button" @click="updateProfile">更新</button>
@@ -42,22 +42,37 @@ export default {
   mixins: [authMixins],
   data() {
     return {
-      user: {
+      profile: {
         username: null,
         nickname: null,
         email: null,
         birthday: null,
-        sex: '1',
+        sex: '0',
         point: 0,
       },
     };
   },
   methods: {
+    getProfile() {
+      const url = `${process.env.VUE_APP_API}/profile`;
+      this.$http.get(url, this.config)
+        .then((res) => {
+          if (res.status === 200) {
+            this.profile = res.data.data;
+          }
+        })
+        .catch((error) => {
+          const response = error.response;
+          if (response) {
+            console.log(response.data);
+          }
+        });
+    },
     updateProfile() {
       const url = `${process.env.VUE_APP_API}/profile`;
       const payload = {
-        nickname: this.user.nickname,
-        email: this.user.email,
+        nickname: this.profile.nickname,
+        email: this.profile.email,
       };
       this.$http.put(url, payload, this.config)
         .then((res) => {
@@ -81,6 +96,9 @@ export default {
           }
         });
     },
+  },
+  mounted() {
+    this.getProfile();
   },
 };
 </script>
