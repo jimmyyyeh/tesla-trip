b948b1c22e49dcb432f141ff5671d525537e3ef1
Jimmy Yeh <chienfeng0719@gmail.com>
2022-03-18 16:51:20 +0800

    Add product page charger selector

--- tesla-trip/src/views/ProductView.vue
+++ tesla-trip/src/views/ProductView.vue
@@ -7,7 +7,17 @@
   <div class="wrap">
     <div class="container">
       <div class="product-form">
-        <div class="product-selector selector">
+        <div class="selector-group">
+          <div class="charger-selector selector">
+            <label class="selector-label" for="chargers">充電站:</label>
+            <select name="chargers" id="chargers" v-model="chargerIndex">
+              <option v-for="(charger, index) in chargers" :key="index" :value="index">{{
+                  charger
+                }}
+              </option>
+            </select>
+          </div>
+          <div class="product-selector selector">
           <label class="selector-label" for="products">產品:</label>
           <select name="products" id="products" v-model="productIndex">
             <option v-for="(product, index) in products" :key="index" :value="index">{{
@@ -15,6 +25,7 @@
               }}
             </option>
           </select>
+        </div>
         </div>
         <div class="overlay" v-show="isLoading">
           <div class="spinner-grow"></div>
@@ -48,6 +59,10 @@ export default {
       product: '請選擇',
       products: ['請選擇'],
       productMap: {},
+      chargerIndex: 0,
+      charger: '請選擇',
+      chargers: ['請選擇'],
+      chargerMap: {},
     };
   },
   watch: {
@@ -55,6 +70,14 @@ export default {
       if (value !== 0) {
         const productID = this.productMap[value - 1];
         this.getQrCode(productID);
+      } else {
+        this.image = null;
+      }
+    },
+    chargerIndex(value) {
+      if (value !== 0) {
+        const chargerID = this.chargerMap[value - 1];
+        this.getProducts(chargerID);
       } else {
         this.image = null;
       }
@@ -80,8 +103,8 @@ export default {
           }
         });
     },
-    getProducts() {
-      const url = `${process.env.VUE_APP_API}/product`;
+    getProducts(chargerID) {
+      const url = `${process.env.VUE_APP_API}/product?charger_id=${chargerID}`;
       this.$http.get(url, this.config)
         .then((res) => {
           if (res.status === 200) {
@@ -101,8 +124,27 @@ export default {
           }
         });
     },
+    getChargers() {
+      const url = `${process.env.VUE_APP_API}/super-charger`;
+      this.$http.get(url, this.config)
+        .then((res) => {
+          if (res.status === 200) {
+            const dataList = res.data.data;
+            dataList.forEach((data, index) => {
+              this.chargers.push(`${data.city}-${data.name}`);
+              this.chargerMap[index] = data.id;
+            });
+          }
+        })
+        .catch((error) => {
+          const response = error.response;
+          if (response) {
+            console.log(response.data);
+          }
+        });
+    },
     initData() {
-      this.getProducts();
+      this.getChargers();
     },
   },
   created() {
