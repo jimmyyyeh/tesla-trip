77c3f40f77ccffc5c638d426a1534e4a7aaa3acf
Jimmy Yeh <chienfeng0719@gmail.com>
2022-03-22 15:28:55 +0800

    Init home page content

--- tesla-trip/src/views/HomeView.vue
+++ tesla-trip/src/views/HomeView.vue
@@ -1,11 +1,44 @@
 <template>
   <div class="offcanvas-bar">
-    <input class="offcanvas-button" type="image" src="https://i.imgur.com/um46tbY.png" data-bs-toggle="offcanvas" data-bs-target="#offcanvas" alt="offcanvas">
+    <input class="offcanvas-button" type="image" src="https://i.imgur.com/um46tbY.png"
+           data-bs-toggle="offcanvas" data-bs-target="#offcanvas" alt="offcanvas">
   </div>
   <OffCanvas></OffCanvas>
   <NavTab></NavTab>
   <div class="wrap">
-    <div class="container">
+    <div class="container home-container">
+      <div class="content">
+        <div class="display">
+          <img src="https://i.imgur.com/tDs7NCD.png" alt="trip">
+          <div class="description">
+            <h1>紀錄旅程</h1>
+            <br>
+            <h2>分享並記錄你的旅程 並賺取更多的旅途幣</h2>
+          </div>
+        </div>
+        <div class="display">
+          <img src="https://i.imgur.com/n9MaXj3.png" alt="redeem">
+          <div class="description">
+            <h1>旅途幣</h1>
+            <br>
+            <h2>透過旅途幣來換杯美味咖啡 享受充電時光</h2>
+          </div>
+        </div>
+        <div class="display">
+          <img src="https://i.imgur.com/oAL4DQ5.png" alt="rate">
+          <div class="description">
+            <h1>讚賞車友</h1>
+            <br>
+            <h2>接受到分享旅程的幫助 回饋給車友對應的旅途幣</h2>
+          </div>
+        </div>
+        <div class="display">
+          <img src="https://i.imgur.com/xvNN5rP.png" alt="tesla">
+          <div class="description">
+            <button @click="goRegister">馬上加入</button>
+          </div>
+        </div>
+      </div>
       <div class="footer">
         <div class="icons">
           <button class="image-button" @click="openSocialMedia('facebook')">
@@ -27,8 +60,12 @@
       </div>
     </div>
   </div>
-  <AlertModal ref="alertModal" :title="alert.title" :message="alert.message" :isCancelShow="alert.isCancelShow" :confirmFunction="alert.confirmFunction"></AlertModal>
-  <AlertModal ref="confirmModal" :title="alert.title" :message="alert.message" :isCancelShow="alert.isCancelShow" :confirmFunction="alert.confirmFunction"></AlertModal>
+  <AlertModal ref="alertModal" :title="alert.title" :message="alert.message"
+              :isCancelShow="alert.isCancelShow"
+              :confirmFunction="alert.confirmFunction"></AlertModal>
+  <AlertModal ref="confirmModal" :title="alert.title" :message="alert.message"
+              :isCancelShow="alert.isCancelShow"
+              :confirmFunction="alert.confirmFunction"></AlertModal>
 </template>
 
 <script>
@@ -48,6 +85,9 @@ export default {
     },
     sendEmail() {
       window.location.href = 'mailto:chienfeng0719@hotmail.com';
+    },
+    goRegister() {
+      this.$router.push('/auth');
     },
   },
 };
