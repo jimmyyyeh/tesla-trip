261f2fd2cf1766cd07c0e497ec4141fb4482b652
Jimmy Yeh <chienfeng0719@gmail.com>
2022-05-13 17:09:44 +0800

    Hide trip coin related functions in offcanvas

--- tesla-trip/src/components/OffCanvas.vue
+++ tesla-trip/src/components/OffCanvas.vue
@@ -7,8 +7,8 @@
       <ul>
         <li @click="redirectPage('home')" data-bs-dismiss="offcanvas">首頁</li>
         <li @click="redirectPage('trip')" data-bs-dismiss="offcanvas">旅途里程</li>
-        <li @click="redirectPage('product')" data-bs-dismiss="offcanvas">點數兌換</li>
-        <li @click="redirectPage('stock')" data-bs-dismiss="offcanvas">庫存管理</li>
+<!--        <li @click="redirectPage('product')" data-bs-dismiss="offcanvas">點數兌換</li>-->
+<!--        <li @click="redirectPage('stock')" data-bs-dismiss="offcanvas">庫存管理</li>-->
         <li @click="redirectPage('profile')" data-bs-dismiss="offcanvas">個人檔案</li>
         <li @click="redirectPage('auth')" data-bs-dismiss="offcanvas">{{ $parent.isSignIn ? '登出': '登入' }}</li>
       </ul>
